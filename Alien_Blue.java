4b580f2883914e78a1bd938fae27e38bd590304d
Iewandowski <66881874+Iewandowski@users.noreply.github.com>
2020-11-26 17:55:01 -0300

    Add files via upload
    edit alien azul (dois tiros pra eliminar)

--- Alien_Blue.java
+++ Alien_Blue.java
@@ -3,7 +3,8 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.Paint;
 
 public class Alien_Blue extends Alien{
-	Image img = new Image("Enemy2.png");
+	int hit = 0;
+	Image img = new Image("Enemy2.gif");
 	public Alien_Blue(int px, int py) {
 		super(px, py);
 	}
@@ -11,12 +12,32 @@ public class Alien_Blue extends Alien{
 	@Override
 	public void start() {
 		setDirH(1);
-		setSpeed(6);
+		setSpeed(1);
 	}
+	
+	@Override
+    public void Update(){		
+		if (jaColidiu()){
+			hit++;
+		}
+		if (hit == 20 ) {
+			deactivate();
+		}
+       
+        	setPosX(getX() + getDirH() * getSpeed()); 	
+        	// Se chegou no lado direito da tela ...	            
+            if (getX() >= getLMaxH()){	
+                // Reposiciona no lado esquerdo e ...
+                setPosX(getLMinH());
+                // Sorteia o passo de avanço [1,5]	                
+                setSpeed(Params.getInstance().nextInt(5)+1);  
+            
+        }
+    }
 
 	@Override
 	public void Draw(GraphicsContext graphicsContext) {
-		graphicsContext.drawImage(img, getX(), getY(), 32, 32);
+		graphicsContext.drawImage(img, getX(), getY(), 32, 52);  
 	}
 
 	@Override
